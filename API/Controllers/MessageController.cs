9b43eb5443c97f6da2c1eeb47d2cc9281c3437bc
ArnT81 <anders_soderberg@hotmail.com>
2023-11-24 10:40:33 +0100

    Removed Weather example and added CRUD for messages

--- API/Controllers/MessageController.cs
+++ API/Controllers/MessageController.cs
@@ -16,18 +16,60 @@ namespace API.Controllers
             _db = db;
         }
 
+
+        //GET
         [HttpGet("messages")]
         [ProducesResponseType(200)]
-
         public IEnumerable<Message> Get()
         {
             LoadMockedDataIfTableIsEmpty();
-            var messages = _db.Messages.ToList();
-            return messages;
+            return _db.Messages.ToList();
         }
 
 
+        //GET (BY ID)
+        [HttpGet("messages/{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public Message Get(int id)
+        {
+            return _db.Messages.Where(message => message.Id == id).FirstOrDefault();
+        }
 
+
+        //POST
+        [HttpPost("messages")]
+        [ProducesResponseType(201)]
+        public void Post([FromBody] Message value)
+        {
+            //AUTOINCREMENTS ID AUTOMATICALLY IF NOT SENT
+            _db.Add(value);
+            _db.SaveChanges();
+        }
+
+
+        //PUT
+        [HttpPut("messages/{id}")]
+        [ProducesResponseType(200)]
+        public void Put(int id, [FromBody] Message value)
+        {
+            _db.Remove(_db.Messages.Where(message => message.Id == id).FirstOrDefault());
+            _db.Add(value);
+            _db.SaveChanges();
+        }
+
+
+        //DELETE
+        [HttpDelete("messages/{id}")]
+        [ProducesResponseType(200)]
+        public void Delete(int id)
+        {
+            _db.Remove(_db.Messages.Where(message => message.Id == id).FirstOrDefault());
+            _db.SaveChanges();
+        }
+
+
+        //CREATE MOCKED DATA IF TABLE IS EMPTY
         private void LoadMockedDataIfTableIsEmpty()
         {
             if (_db.Messages.Count() == 0)
